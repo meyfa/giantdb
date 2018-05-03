bfeaa20fb29b5094cb40ac4fdd5954eeee238554
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2018-05-03 16:08:41 +0200

    Update Change to use writable-wrapper package

--- test/change.test.js
+++ test/change.test.js
@@ -1,7 +1,5 @@
 "use strict";
 
-const Promise = require("bluebird");
-
 const chai = require("chai");
 chai.use(require("chai-as-promised"));
 const expect = chai.expect;
@@ -25,117 +23,6 @@ describe("lib/change.js", function () {
         return expect(obj.committed).to.be.false;
     });
 
-    it("forwards errors", function (done) {
-        const out = new PassThrough();
-        const obj = new Change("foo", out, () => {});
-        const error = new Error("oops!");
-        obj.on("error", function (err) {
-            expect(err).to.equal(error);
-            done();
-        });
-        out.emit("error", error);
-    });
-
-    describe("#write()", function () {
-
-        it("writes data to the underlying stream", function (done) {
-            const out = new PassThrough();
-            const obj = new Change("foo", out, () => {});
-            obj.write("hello world", "utf8");
-            out.on("data", function (chunk) {
-                const expected = Buffer.from("hello world", "utf8");
-                expect(chunk).to.satisfy((c) => expected.equals(c));
-                done();
-            });
-        });
-
-    });
-
-    describe("#end()", function () {
-
-        it("writes data to the underlying stream", function (done) {
-            const out = new PassThrough();
-            const obj = new Change("foo", out, () => {});
-            obj.end("hello world", "utf8");
-            out.on("data", function (chunk) {
-                const expected = Buffer.from("hello world", "utf8");
-                expect(chunk).to.satisfy((c) => expected.equals(c));
-                done();
-            });
-        });
-
-        it("ends the underlying stream", function (done) {
-            const out = new DevNull();
-            const obj = new Change("foo", out, () => {});
-            obj.end();
-            out.on("finish", done);
-        });
-
-        it("invokes the committer", function (done) {
-            const out = new DevNull();
-            const obj = new Change("foo", out, () => done());
-            obj.end();
-        });
-
-        it("passes the id to the committer", function (done) {
-            const out = new DevNull();
-            const obj = new Change("foo", out, (id) => {
-                expect(id).to.equal("foo");
-                done();
-            });
-            obj.end();
-        });
-
-        it("ends the output before emitting 'finish'", function (done) {
-            const out = new DevNull();
-            let outFinish = false;
-            const obj = new Change("foo", out, () => {});
-            out.on("finish", function () {
-                outFinish = true;
-            });
-            obj.on("finish", function () {
-                expect(outFinish).to.be.true;
-                done();
-            });
-            obj.end();
-        });
-
-        it("commits before emitting 'finish'", function (done) {
-            const out = new DevNull();
-            let committed = false;
-            const obj = new Change("foo", out, () => {
-                committed = true;
-            });
-            obj.on("finish", function () {
-                expect(committed).to.be.true;
-                done();
-            });
-            obj.end();
-        });
-
-        it("sets 'committed' to true", function (done) {
-            const out = new DevNull();
-            const obj = new Change("foo", out, () => {});
-            obj.on("finish", function () {
-                expect(obj.committed).to.be.true;
-                done();
-            });
-            obj.end();
-        });
-
-        it("emits an error when the committer fails", function (done) {
-            const out = new DevNull();
-            const obj = new Change("foo", out, () => {
-                return Promise.reject(new Error("oops!"));
-            });
-            obj.on("error", function () {
-                done();
-            });
-            obj.end();
-        });
-
-    });
-
     describe("#commit()", function () {
 
         it("ends itself", function (done) {
@@ -149,6 +36,21 @@ describe("lib/change.js", function () {
             const out = new DevNull();
             const obj = new Change("foo", out, () => {});
             return expect(obj.commit()).to.eventually.be.fulfilled;
+        });
+
+        it("invokes the committer", function (done) {
+            const out = new DevNull();
+            const obj = new Change("foo", out, () => done());
+            obj.commit();
+        });
+
+        it("passes the id to the committer", function (done) {
+            const out = new DevNull();
+            const obj = new Change("foo", out, (id) => {
+                expect(id).to.equal("foo");
+                done();
+            });
+            obj.commit();
         });
 
         it("resolves to the committer's return value", function () {
@@ -165,13 +67,17 @@ describe("lib/change.js", function () {
         });
 
         it("rejects when the underlying stream fails", function () {
-            const out = new PassThrough({
-                final: function (callback) {
-                    callback(new Error("oops!"));
-                },
-            });
+            const out = new PassThrough();
+            out.end = () => out.emit("error", new Error("oops!"));
             const obj = new Change("foo", out, () => {});
             return expect(obj.commit()).to.eventually.be.rejected;
+        });
+
+        it("sets 'committed' to true", function () {
+            const out = new DevNull();
+            const obj = new Change("foo", out, () => {});
+            obj.commit();
+            expect(obj.committed).to.be.true;
         });
 
     });
