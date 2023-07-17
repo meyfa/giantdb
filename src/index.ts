6bcafd41aa55e8da8f6ac4efbaae0774f9b74566
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2023-07-17 20:09:46 +0200

    fix: Export types with "export type" for clarity

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { GiantDB } from './giantdb.js'
 export { Item } from './item.js'
 export { Change } from './change.js'
-export { Middleware, MiddlewareTransformFn, MiddlewareNextFn, MiddlewareResult } from './middleware/middleware.js'
+export type { Middleware, MiddlewareTransformFn, MiddlewareNextFn, MiddlewareResult } from './middleware/middleware.js'
